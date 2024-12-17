87a71e33e39907c626992f58472236ca576a590f
Vijay <rebalvijay930@gmail.com>
2024-12-17 11:00:43 +0530

    phase one

---
+++ server.js
@@ -0,0 +1,12 @@
+const express=require('express')
+
+const app=express();
+
+
+app.use('/user',(req,res)=>{
+    
+})
+
+app.listen(7777,()=>{
+    console.log('Server is successfully listening on port 7777...')
+})
