23cea57c449c5b7151a583057b38ccc8a9060390
Vijay <rebalvijay930@gmail.com>
2024-12-17 23:52:47 +0530

    changes in database

--- server.js
+++ server.js
@@ -1,19 +1,37 @@
 const express=require('express')
-
+const connectDB=require('./config/database');
 const app=express();
+const User=require('./models/user')
 
 
-app.use('/user',(req,res,next)=>{
-    console.log("Handleing the route user!!")
-    res.send("Response");
-    next();
-},(req,res)=>{
-    console.log("Handleing the route user 2!!")
-    res.send("2nd Response")
+app.post("/signup",async(req,res)=>{
+    const usreObj={
+        firstName:"virat",
+        lastName:"kohli",
+        emailId:"viratkohli@gmail.com",
+        password:"123456"
+
+    }
+
+    //Creating a new instance of the User model
+    const user=new User(usreObj);
+    
+    await user.save();
+    res.send("User Added Successfully")
+
 })
 
-app.listen(7777,()=>{
-    console.log('Server is successfully listening on port 7777...')
-})
+
+connectDB()
+    .then(()=>{
+        console.log("Database connection established...")
+        app.listen(7777,()=>{
+            console.log('Server is successfully listening on port 7777...')
+        })
+    })
+    .catch((error)=>{
+        console.log("Database cannot be connected!!")
+    })
+
 
 
