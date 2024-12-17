041cddfd89a678eedc5266d5adc5e5488fb506dd
Vijay <rebalvijay930@gmail.com>
2024-12-17 22:24:57 +0530

    phase one

--- server.js
+++ server.js
@@ -3,10 +3,17 @@ const express=require('express')
 const app=express();
 
 
-app.use('/user',(req,res)=>{
-    
+app.use('/user',(req,res,next)=>{
+    console.log("Handleing the route user!!")
+    res.send("Response");
+    next();
+},(req,res)=>{
+    console.log("Handleing the route user 2!!")
+    res.send("2nd Response")
 })
 
 app.listen(7777,()=>{
     console.log('Server is successfully listening on port 7777...')
 })
+
+
